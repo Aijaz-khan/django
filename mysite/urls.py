77b3ea330286153b45d9338c4439d0c616701f86
Aijaz Khan <aijaz-khan@hotmail.com>
2016-11-24 14:50:38 +0500

    Steps added and More django features explored

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 urlpatterns = [
     url(r'^courses/', include('courses.urls')),
