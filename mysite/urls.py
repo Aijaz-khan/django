8699e832eb0c9316626bd13c0bd94c92fa3d4145
Aijaz Khan <aijaz-khan@hotmail.com>
2016-11-25 18:07:17 +0500

    Links added Namespace added

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 urlpatterns = [
-    url(r'^courses/', include('courses.urls')),
+    url(r'^courses/', include('courses.urls', namespace='courses')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.hello_world),
 ]
