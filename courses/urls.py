a86f6baecfbb1044e867faaa02132d7e98c64574
Aijaz Khan <aijaz-khan@hotmail.com>
2016-12-01 11:46:54 +0500

    Updated, Delete and Add features added

--- courses/urls.py
+++ courses/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
 url(r'^$', views.course_list, name= 'list'),
+url(r'^create/$', views.course_create, name= 'create'),
 url(r'(?P<course_pk>\d+)/(?P<step_pk>\d+)/$', views.step_detail, name='step'),
 url(r'(?P<pk>\d+)/$', views.course_detail, name='detail'),
+url(r'(?P<pk>\d+)/edit/$', views.course_update, name='update'),
+url(r'(?P<pk>\d+)/delete/$', views.course_delete, name='update'),
 
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,13 +1,42 @@
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, render
 
 from .models import Course, Step
+from .forms import CourseForm
+
+
+def course_create(request):
+    form = CourseForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save
+    context = {
+        "form": form,
+    }
+    return render(request, "courses/course_form.html", context)
+
+def course_update(request, pk):
+    instance = get_object_or_404(Course, pk=pk)
+    form = CourseForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "title": instance.title,
+        "description": instance.description,
+        "form": form,
+    }
+    return render(request, "courses/course_form.html", context)
+
+def course_delete(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+    course.delete()
 
 def course_list(request):
     courses = Course.objects.all()
     return render(request, 'courses/course_list.html', {'courses': courses})
-
 
 def course_detail(request, pk):
     course = get_object_or_404(Course, pk=pk)
